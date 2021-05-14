23a9a40809a85d4abd6f3f4b722172c8e98217cc
Quentin Gruber <quentingruber@gmail.com>
2021-05-14 19:30:16 +0200

    Update DayCard.tsx

--- src/components/DayCard/DayCard.tsx
+++ src/components/DayCard/DayCard.tsx
@@ -5,30 +5,38 @@ import { Card, CardHeader, CardContent, CardActions, IconButton, Typography } fr
 interface DayCardProps {
     dailyData:any;
 }
+
+function convertToDegreCelsius(tempInkalvin:number):string{
+  return `${(tempInkalvin - 273.15).toFixed(1)}°`
+};
+
 export default function DayCard(props:DayCardProps){
     console.log(props.dailyData)
+    const { wind_speed:wind, humidity , rain , pressure , sunrise , sunset , temp:{day:dayTemp, morn:mornTemp}, feels_like:{day:dayFeelsLike, morn:mornFeelsLike}} = props.dailyData
+    const weatherDescription = props.dailyData.weather[0].description
     return( <Card style={{width:"300px"}}>
     <CardHeader
-      title={"cityData.name"}
-      subheader={"this.convertToDegreCelsius(cityData.main.temp)"}
+      title={"DATE"}
     />
     <CardContent >
     <img
     style={{width: "150px"}}
       src={process.env.PUBLIC_URL+"assets/cloudy_dark.png"}
     />
-    <h6>cloud</h6>
+    <h6>{weatherDescription}</h6>
     </CardContent>
      <CardContent>
-      {"this.convertToDegreCelsius(cityData.main.temp)"}
-      <Typography>
-      Feels Like : {"this.convertToDegreCelsius(cityData.main.feels_like)"}
+     <Typography>
+      Morning : {convertToDegreCelsius(mornTemp)} Feels Like : {convertToDegreCelsius(mornFeelsLike)}
       </Typography>
       <Typography>
-      Min : {"this.convertToDegreCelsius(cityData.main.temp_min)"}
+      Day : {convertToDegreCelsius(dayTemp)} Feels Like : {convertToDegreCelsius(dayFeelsLike)}
       </Typography>
       <Typography>
-      Max : {"this.convertToDegreCelsius(cityData.main.temp_max)"}
+      Pressure : {pressure} Humidity : {humidity} Wind :{wind} Sunrise: {sunrise} Sunset: {sunset}
+      </Typography>
+      <Typography>
+      Rain : {rain}mm
       </Typography>
     </CardContent>
   </Card>)
